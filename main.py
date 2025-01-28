690bf44945b70d82591790bfd82f180e5434b52d
Gabriel Santos <gabriel.santos@amf.edu.br>
2025-01-28 16:28:42 -0300

    Estrutura

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from rembg import remove
 from PIL import Image
 
-
 def remove_background(input_path: str, output_path: str) -> None:
     try:
         with Image.open(input_path) as img:
-            
             img_no_bg = remove(img)
-            
             
             img_no_bg.save(output_path)
             print(f"Imagem salva com sucesso em: {output_path}")
@@ -17,6 +14,5 @@ def remove_background(input_path: str, output_path: str) -> None:
 if __name__ == "__main__":
     input_path = ""
     output_path = ""
-
     
     remove_background(input_path, output_path)
