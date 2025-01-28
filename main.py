49fb7ec404b9cd5f21b3683f9b3b7f6686917ebf
Gabriel Santos <gabriel.santos@amf.edu.br>
2025-01-28 16:25:05 -0300

    REMOVE BG

---
+++ main.py
@@ -0,0 +1,22 @@
+from rembg import remove
+from PIL import Image
+
+
+def remove_background(input_path: str, output_path: str) -> None:
+    try:
+        with Image.open(input_path) as img:
+            
+            img_no_bg = remove(img)
+            
+            
+            img_no_bg.save(output_path)
+            print(f"Imagem salva com sucesso em: {output_path}")
+    except Exception as e:
+        print(f"Erro ao processar a imagem: {e}")
+
+if __name__ == "__main__":
+    input_path = ""
+    output_path = ""
+
+    
+    remove_background(input_path, output_path)
